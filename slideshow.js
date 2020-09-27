ae85b1fc8847e3fb97049f4089d5c3f4886a2703
Marcin Wojcik <marcincyniu@gmail.com>
2020-09-27 16:46:29 -0700

    add first files

---
+++ slideshow.js
@@ -0,0 +1,41 @@
+showFiles = () => {
+    let fileObjects = document.getElementById('filePicker');
+
+    hideManageDiv();
+
+    //console.log(fileObjects.files);
+    let idx = 0;
+    let noOfElements = fileObjects.files.length;
+    let imagesContainer = document.getElementById('imagesContainer');
+
+    let id = setInterval(() => {
+            //Get new image path:
+            let imagePath = fileObjects.files[idx].webkitRelativePath;
+            console.log(imagePath);
+
+            //Create new image element:
+            let element = document.createElement('img');
+            element.src = '/home/developer/' + imagePath;
+            element.style.maxHeight = '100%';
+            element.style.maxWidth = '100%';
+            element.id = 'imege_' + idx;
+
+            //Remove already existing image:
+            if (idx > 0) imagesContainer.removeChild(imagesContainer.childNodes[0]);
+
+            //Display new image:
+            imagesContainer.appendChild(element);
+
+            //Bump interator:
+            idx++;
+            //When idx equals to number of elements then break/finish interval:
+            if (idx === noOfElements) clearInterval(id);
+        },
+        5000);
+}
+
+hideManageDiv = () => {
+    hideManageDiv
+    let manageDiv = document.getElementById('manageDiv');
+    manageDiv.style.display = 'none';
+}
